08fb9793f791c5a90f39f14d48e04be6a15fcff6
Joe Noel <joe@joenoel.co.uk>
2021-05-21 15:50:27 +0100

    Port engine logic

--- src/audio/buffer.rs
+++ src/audio/buffer.rs
@@ -1,8 +1,42 @@
+pub struct SampleLocation {
+    pub channel: usize,
+    pub frame: usize,
+}
+
 pub trait AudioBuffer {
     fn num_channels(&self) -> usize;
     fn num_frames(&self) -> usize;
     fn sample_rate(&self) -> u32;
     fn clear(&mut self);
+
+    fn set_sample(&mut self, sample_location: &SampleLocation, value: f32);
+    fn add_sample(&mut self, sample_location: &SampleLocation, value: f32);
+    fn get_sample(&self, sample_location: &SampleLocation) -> f32;
+
+    fn add_from(
+        &mut self,
+        source_buffer: &dyn AudioBuffer,
+        source_location: &SampleLocation,
+        destination_location: &SampleLocation,
+        num_channels: usize,
+        num_frames: usize,
+    ) {
+        for frame in 0..num_frames {
+            for channel in 0..num_channels {
+                let source = SampleLocation {
+                    channel: channel + source_location.channel,
+                    frame: frame + source_location.frame,
+                };
+
+                let dest = SampleLocation {
+                    channel: channel + destination_location.channel,
+                    frame: frame + destination_location.frame,
+                };
+
+                self.set_sample(&dest, source_buffer.get_sample(&source));
+            }
+        }
+    }
 }
 
 pub struct BorrowedAudioBuffer<'a> {
@@ -38,5 +72,86 @@ impl<'a> AudioBuffer for BorrowedAudioBuffer<'a> {
         for value in self.data.iter_mut() {
             *value = 0.0;
         }
+    }
+
+    fn set_sample(&mut self, sample_location: &SampleLocation, value: f32) {
+        assert!(sample_location.channel < self.num_channels);
+        assert!(sample_location.frame < self.num_frames());
+        self.data[sample_location.frame * self.num_channels + sample_location.channel] = value;
+    }
+
+    fn add_sample(&mut self, sample_location: &SampleLocation, value: f32) {
+        let value_before = self.get_sample(sample_location);
+        self.set_sample(sample_location, value + value_before)
+    }
+
+    fn get_sample(&self, sample_location: &SampleLocation) -> f32 {
+        assert!(sample_location.channel < self.num_channels);
+        assert!(sample_location.frame < self.num_frames());
+        self.data[sample_location.frame * self.num_channels + sample_location.channel]
+    }
+}
+
+pub struct AudioBufferSlice<'a> {
+    buffer: &'a mut dyn AudioBuffer,
+    offset: usize,
+    num_frames: usize,
+}
+
+impl<'a> AudioBufferSlice<'a> {
+    pub fn new(buffer: &'a mut dyn AudioBuffer, offset: usize, num_frames: usize) -> Self {
+        if offset >= buffer.num_frames() {
+            panic!("Invalid offset");
+        }
+
+        Self {
+            buffer,
+            offset,
+            num_frames,
+        }
+    }
+
+    fn translate_location(&self, sample_location: &SampleLocation) -> SampleLocation {
+        SampleLocation {
+            channel: sample_location.channel,
+            frame: sample_location.frame + self.offset,
+        }
+    }
+}
+
+impl<'a> AudioBuffer for AudioBufferSlice<'a> {
+    fn num_channels(&self) -> usize {
+        self.buffer.num_channels()
+    }
+
+    fn num_frames(&self) -> usize {
+        self.buffer.num_frames() - self.offset
+    }
+
+    fn sample_rate(&self) -> u32 {
+        self.buffer.sample_rate()
+    }
+
+    fn clear(&mut self) {
+        for frame in 0..self.num_frames {
+            for channel in 0..self.num_channels() {
+                self.set_sample(&SampleLocation { channel, frame }, 0.0);
+            }
+        }
+    }
+
+    fn set_sample(&mut self, sample_location: &SampleLocation, value: f32) {
+        let new_location = self.translate_location(sample_location);
+        self.buffer.set_sample(&new_location, value)
+    }
+
+    fn add_sample(&mut self, sample_location: &SampleLocation, value: f32) {
+        let new_location = self.translate_location(sample_location);
+        self.buffer.add_sample(&new_location, value)
+    }
+
+    fn get_sample(&self, sample_location: &SampleLocation) -> f32 {
+        let new_location = self.translate_location(sample_location);
+        self.buffer.get_sample(&new_location)
     }
 }
--- src/audio/engine.rs
+++ src/audio/engine.rs
@@ -1,11 +1,12 @@
 use super::{
-    buffer::AudioBuffer,
+    buffer::{AudioBuffer, AudioBufferSlice, SampleLocation},
     command::{Command, QueueCommand},
     notification::Notification,
     timeline::Timeline,
 };
 use crate::{
     model::{
+        id::ID,
         playback_state::{PlaybackState, PlayingState},
         project::Project,
         sample::Sample,
@@ -15,7 +16,7 @@ use crate::{
     types::beats::Beats,
 };
 use futures_channel::mpsc::{Receiver, Sender};
-use std::{convert::TryInto, mem};
+use std::{cmp::min, convert::TryInto, mem, usize};
 
 pub trait Engine {
     fn render<T>(&mut self, output: &mut T)
@@ -32,6 +33,7 @@ pub struct AudioEngine {
     timeline: Timeline,
     last_section_start: Beats,
     loop_count: i32,
+    sample_rate: f64,
 }
 
 impl AudioEngine {
@@ -45,6 +47,7 @@ impl AudioEngine {
             timeline: Timeline::new(44100),
             last_section_start: Beats::from_num(0.0),
             loop_count: 0,
+            sample_rate: 44100.0,
         }
     }
 
@@ -132,11 +135,164 @@ impl AudioEngine {
         self.playback_state.queued_section_id = Some(command.section_id);
     }
 
-    fn process_project<T>(&self, output: &mut T) -> bool
+    fn buffer_for_sample(&self, sample_id: &ID) -> Option<&dyn AudioBuffer> {
+        None
+    }
+
+    fn next_sample_after(&self, beat_position: Beats, bpm: f64) -> usize {
+        let beat_frequency = bpm / 60.0;
+        let sample_position = (beat_position.to_num::<f64>() * self.sample_rate / beat_frequency).ceil();
+        sample_position as usize
+    }
+
+    fn increment_section(&mut self) {
+        self.loop_count += 1;
+
+        if let Some(queued_song_id) = self.playback_state.queued_song_id {
+            if let Some(queued_section_id) = self.playback_state.queued_section_id {
+                self.playback_state.song_id = Some(queued_song_id);
+                self.playback_state.section_id = Some(queued_section_id);
+                self.playback_state.queued_section_id = None;
+                self.playback_state.queued_song_id = None;
+
+                if let Some(queued_section) = self.current_section() {
+                    self.playback_state.looping = queued_section.looping;
+                }
+            }
+        }
+
+        if self.playback_state.looping {
+            return;
+        }
+
+        let song = match self.current_song() {
+            Some(song) => song,
+            None => return,
+        };
+
+        let current_section_id = match self.playback_state.section_id {
+            Some(id) => id,
+            None => return,
+        };
+
+        let mut iter = song.section_ids.iter();
+        iter.find(|song_section_id| *song_section_id == &current_section_id);
+        self.playback_state.section_id = iter.next().cloned();
+        self.playback_state.looping = match self.current_section() {
+            Some(section) => section.looping,
+            None => false,
+        };
+    }
+
+    fn update_tempo(&mut self, at_beat_position: Beats) {
+        let tempo = self.current_sample().map(|sample| sample.tempo.bpm);
+
+        if let Some(tempo) = tempo {
+            let sample_position = self.timeline.sample_position_for_beats(at_beat_position).ceil() as i64;
+            self.timeline.set_reference_point(at_beat_position, sample_position);
+            self.timeline.set_tempo(tempo);
+        }
+    }
+
+    fn process_current_section<T>(&mut self, output: &mut T, start_position: Beats, end_position: Beats) -> usize
+    where
+        T: AudioBuffer,
+    {
+        let section = match self.current_section() {
+            Some(section) => section,
+            None => return 0,
+        };
+
+        let sample = match self.current_sample() {
+            Some(sample) => sample,
+            None => return 0,
+        };
+
+        let sample_buffer = match self.buffer_for_sample(&sample.id) {
+            Some(buffer) => buffer,
+            None => return 0,
+        };
+
+        let start_position_in_section = start_position - self.last_section_start;
+        let section_length = Beats::from_num(section.beat_length);
+        let end_position_in_section = min(section_length, end_position - self.last_section_start);
+
+        let start_frame_offset = self.next_sample_after(
+            start_position_in_section + Beats::from_num(section.start),
+            sample.tempo.bpm,
+        );
+
+        let end_frame_offset = self.next_sample_after(
+            end_position_in_section + Beats::from_num(section.start),
+            sample.tempo.bpm,
+        );
+
+        let num_frames = min(end_frame_offset - start_frame_offset, sample_buffer.num_frames());
+        let num_frames = min(num_frames, output.num_frames() - start_frame_offset);
+        let num_channels = min(sample_buffer.num_channels(), output.num_channels());
+
+        let source_location = SampleLocation {
+            channel: 0,
+            frame: start_frame_offset,
+        };
+
+        let destination_location = SampleLocation { channel: 0, frame: 0 };
+
+        output.add_from(
+            sample_buffer,
+            &source_location,
+            &destination_location,
+            num_channels,
+            num_frames,
+        );
+
+        if end_position_in_section == section_length {
+            self.increment_section();
+            self.update_tempo(end_position_in_section + self.last_section_start);
+            self.last_section_start += section_length;
+        }
+
+        num_frames
+    }
+
+    fn process_project<T>(&mut self, output: &mut T) -> bool
     where
         T: AudioBuffer,
     {
         output.clear();
+
+        if self.playback_state.playing != PlayingState::Playing {
+            return false;
+        }
+
+        let mut output_offset: usize = 0;
+
+        while output_offset < output.num_frames() {
+            let remaining = output.num_frames() - output_offset;
+
+            let mut output_slice = AudioBufferSlice::new(output, output_offset, remaining);
+
+            let start_beat_position = self
+                .timeline
+                .beat_position_for_samples((self.sample_position + output_offset).try_into().unwrap());
+            let end_beat_position = self
+                .timeline
+                .beat_position_for_samples((self.sample_position + output_offset + remaining).try_into().unwrap());
+
+            let mut num_frames_processed =
+                self.process_current_section(&mut output_slice, start_beat_position, end_beat_position);
+
+            if num_frames_processed == 0 {
+                num_frames_processed = self.process_current_section(output, start_beat_position, end_beat_position);
+
+                if num_frames_processed == 0 {
+                    return false;
+                }
+            }
+
+            output_offset += num_frames_processed;
+        }
+
         true
     }
 
