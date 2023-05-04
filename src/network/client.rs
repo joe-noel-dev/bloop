e5823f9337e2e551bbaffcc7dd338436ad7d71ae
Joe Noël <joe.noel@focusrite.com>
2023-05-04 14:46:02 +0100

    Refactor network client with struct

--- src/network/client.rs
+++ src/network/client.rs
@@ -1,105 +1,136 @@
 use super::error;
 use crate::api::{Request, Response};
-use futures::{Sink, SinkExt};
+use futures::SinkExt;
+use futures_util::stream::{SplitSink, SplitStream};
 use futures_util::StreamExt;
-use std::marker::Unpin;
+use std::net::SocketAddr;
 use tokio::net::TcpStream;
+use tokio::select;
 use tokio::sync::{broadcast, mpsc};
+use tokio_tungstenite::{accept_async, WebSocketStream};
 use tungstenite::protocol::Message;
 
-extern crate bson;
-extern crate serde;
-extern crate serde_derive;
+struct Client {
+    request_tx: mpsc::Sender<Request>,
+    response_rx: broadcast::Receiver<Response>,
+    address: SocketAddr,
+    outgoing: SplitSink<WebSocketStream<TcpStream>, Message>,
+    incoming: SplitStream<WebSocketStream<TcpStream>>,
+}
 
-pub async fn run(socket: TcpStream, request_tx: mpsc::Sender<Request>, mut response_rx: broadcast::Receiver<Response>) {
-    let addr = match socket.peer_addr() {
-        Ok(addr) => addr,
-        Err(_) => {
-            println!("Error getting peer address");
-            return;
-        }
-    };
+impl Client {
+    async fn new(
+        socket: TcpStream,
+        request_tx: mpsc::Sender<Request>,
+        response_rx: broadcast::Receiver<Response>,
+    ) -> Result<Self, String> {
+        let address = socket.peer_addr().expect("Error getting peer address");
 
-    println!("New connection: {addr}");
+        println!("New connection: {address}");
 
-    let ws_stream = match tokio_tungstenite::accept_async(socket).await {
-        Ok(stream) => stream,
-        Err(_) => {
-            println!("Error during WebSocket handshake");
-            return;
-        }
-    };
+        let ws_stream = match accept_async(socket).await {
+            Ok(stream) => stream,
+            Err(_) => {
+                return Err("Error during WebSocket handshake".to_string());
+            }
+        };
 
-    let (mut outgoing, mut incoming) = ws_stream.split();
+        let (outgoing, incoming) = ws_stream.split();
 
-    loop {
-        tokio::select! {
-            Ok(response) = response_rx.recv() => {
-                send_response(response, &mut outgoing).await;
-            },
-            message = incoming.next() => {
+        Ok(Self {
+            request_tx,
+            response_rx,
+            address,
+            outgoing,
+            incoming,
+        })
+    }
 
-                let message = match message {
-                    Some(message) => message,
-                    None => {
-                        println!("Connection closed to client: {addr}");
-                        break;
-                    }
-                };
+    async fn on_incoming_message(
+        &mut self,
+        message: Option<Result<Message, tungstenite::Error>>,
+    ) -> anyhow::Result<()> {
+        let message = match message {
+            Some(message) => message,
+            None => anyhow::bail!("Connection closed to client: {}", self.address),
+        };
 
+        let message = match message {
+            Ok(message) => message,
+            Err(_) => anyhow::bail!("Error receiving from client: {}", self.address),
+        };
 
-                let message = match message {
-                    Ok(message) => message,
-                    Err(_) => {
-                        println!("Error receiving from client: {addr}");
-                        break;
-                    }
-                };
+        let mut message = match message {
+            Message::Binary(message) => message,
+            _ => return Ok(()),
+        };
 
-                let mut message = match message {
-                    Message::Binary(message) => message,
-                    _ => continue
-                };
+        let api_request = match convert_bytes_to_request(&mut message) {
+            Ok(request) => request,
+            Err(error) => {
+                let response = Response::default().with_error(&error.to_string());
+                self.send_response(&response).await;
+                return Ok(());
+            }
+        };
 
-                let api_request = match handle_message(&mut message) {
-                    Ok(request) => request,
-                    Err(error) => {
-                        send_response(Response::default().with_error(&error.to_string()), &mut outgoing).await;
-                        continue;
-                    }
-                };
-                match request_tx.send(api_request).await {
-                    Ok(_) => continue,
-                    Err(_) => {
-                        println!("Client disconnected: {addr}");
-                        break;
-                    }
-                }
-            },
-            else => { break },
-
+        match self.request_tx.send(api_request).await {
+            Ok(_) => Ok(()),
+            Err(_) => anyhow::bail!("Client disconnected: {}", self.address),
         }
     }
 
-    println!("{addr} disconnected");
+    async fn run(&mut self) {
+        loop {
+            select! {
+                Ok(response) = self.response_rx.recv() => {
+                    self.send_response(&response).await;
+                },
+                message = self.incoming.next() => {
+
+                    if let Err(err) = self.on_incoming_message(message).await {
+                        eprintln!("Error from client: {err}");
+                        break;
+                    }
+
+                },
+                else => { break },
+
+            }
+        }
+
+        println!("Client disconnected: {}", self.address);
+    }
+
+    async fn send_response(&mut self, response: &Response) {
+        if let Some(message) = convert_response(response) {
+            let _ = self.outgoing.send(message).await;
+        }
+    }
 }
 
-async fn send_response(response: Response, mut outgoing: impl Sink<Message> + Unpin) {
-    let document = match bson::to_document(&response) {
+pub async fn run(socket: TcpStream, request_tx: mpsc::Sender<Request>, response_rx: broadcast::Receiver<Response>) {
+    match Client::new(socket, request_tx, response_rx).await {
+        Ok(mut client) => client.run().await,
+        Err(error) => eprintln!("Error from client: {error}"),
+    }
+}
+
+fn convert_response(response: &Response) -> Option<Message> {
+    let document = match bson::to_document(response) {
         Ok(doc) => doc,
         Err(error) => {
             println!("Error serialising response: {error}");
-            return;
+            return None;
         }
     };
 
     let mut data: Vec<u8> = vec![];
     document.to_writer(&mut data).unwrap();
-
-    let _ = outgoing.send(Message::binary(data)).await;
+    Some(Message::binary(data))
 }
 
-fn handle_message(message: &mut [u8]) -> Result<Request, error::NetworkError> {
+fn convert_bytes_to_request(message: &mut [u8]) -> Result<Request, error::NetworkError> {
     let document = match bson::Document::from_reader(&mut &message[..]) {
         Ok(doc) => doc,
         Err(error) => {
