f010eedc940d5691276877287f36a166b4e0b408
Joe Noël <joe.noel@focusrite.com>
2023-05-04 11:27:05 +0100

    Add plugin to support top-level awaits

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,16 @@
 import {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import ReactModal from 'react-modal';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 ReactModal.setAppElement('#root');
