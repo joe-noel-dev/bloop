0f178f722643bba3ba235687373bb00c0d991389
Joe Noël <joe.noel@focusrite.com>
2023-05-15 18:08:08 +0100

    Support files at different samples rates

--- src/audio/sampler_converter.rs
+++ src/audio/sampler_converter.rs
@@ -15,18 +15,23 @@ pub struct SampleConversionResult {
 
 pub struct SampleConverter {
     complete_tx: mpsc::Sender<SampleConversionResult>,
+    target_sample_rate: usize,
 }
 
 impl SampleConverter {
-    pub fn new(complete_tx: mpsc::Sender<SampleConversionResult>) -> Self {
-        Self { complete_tx }
+    pub fn new(complete_tx: mpsc::Sender<SampleConversionResult>, target_sample_rate: usize) -> Self {
+        Self {
+            complete_tx,
+            target_sample_rate,
+        }
     }
 
     pub fn convert(&self, sample_id: ID, sample_path: PathBuf) {
         let mut complete_tx = self.complete_tx.clone();
+        let sample_rate = self.target_sample_rate;
 
         std::thread::spawn(move || {
-            let result = convert_sample(&sample_path);
+            let result = convert_sample(&sample_path, sample_rate);
             let _ = complete_tx.try_send(SampleConversionResult { sample_id, result });
         });
     }
