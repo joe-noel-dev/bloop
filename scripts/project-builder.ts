20cb84fe70cf31056c509a442f5457230730d839
Joe Noël <joe.noel@focusrite.com>
2023-05-23 11:50:53 +0100

    Read in samples in project builder script

--- scripts/project-builder.ts
+++ scripts/project-builder.ts
@@ -1,16 +1,79 @@
-import process from 'process';
-import fs from 'fs';
-import {emptyProject} from '../frontend/src/model/project';
+import {Project, emptyProject} from '../frontend/src/model/project';
 import {randomUUID} from 'crypto';
-import {Song} from '../frontend/src/model/song';
+import {Reader as WavReader} from 'wav';
+import {Sample} from '../frontend/src/model/sample';
 import {Section} from '../frontend/src/model/section';
+import {Song} from '../frontend/src/model/song';
 
-const convertSong = (songSpec: any): Song => {
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import process from 'process';
+
+interface WavInfo {
+  sampleRate: number;
+  frames: number;
+  channels: number;
+}
+
+const readWavFile = async (path: string): Promise<WavInfo> => {
+  return new Promise((resolve, reject) => {
+    const wavReader = new WavReader();
+
+    wavReader.on('error', reject);
+
+    wavReader.on('format', (format) => {
+      let frames = 0;
+
+      wavReader.on('data', (data) => {
+        frames += data.length / format.blockAlign;
+      });
+
+      wavReader.on('end', () => {
+        resolve({
+          channels: format.channels,
+          sampleRate: format.sampleRate,
+          frames: Math.round(frames),
+        });
+      });
+    });
+
+    fs.createReadStream(path).pipe(wavReader);
+  });
+};
+
+const convertSample = async (
+  samplePath: string,
+  samplesDir: string,
+  bpm: number
+): Promise<Sample> => {
+  const wavInfo = await readWavFile(samplePath);
+  const sampleId = randomUUID();
+  const destinationPath = path.join(samplesDir, `${sampleId}.wav`);
+  fs.copyFileSync(samplePath, destinationPath);
+  return {
+    id: sampleId,
+    name: 'Sample',
+    tempo: {bpm},
+    sampleCount: wavInfo.frames,
+    sampleRate: wavInfo.sampleRate,
+    channelCount: wavInfo.channels,
+  };
+};
+
+const convertSong = async (
+  songSpec: any,
+  samplesDir: string
+): Promise<Song> => {
+  const bpm = songSpec.tempo ?? 120;
   const song = {
     id: randomUUID(),
     name: songSpec.name ?? 'Song',
-    tempo: {bpm: songSpec.tempo ?? 120},
+    tempo: {bpm},
     sections: songSpec.sections ? songSpec.sections.map(convertSection) : [],
+    sample: songSpec.sample
+      ? await convertSample(songSpec.sample, samplesDir, bpm)
+      : undefined,
   };
 
   song.sections.sort((a: Section, b: Section) => a.start - b.start);
@@ -24,25 +87,63 @@ const convertSection = (sectionSpec: any): Section => {
     name: sectionSpec.name ?? 'Section',
     start: sectionSpec.start ?? 0,
     loop: sectionSpec.loop ?? false,
+    metronome: sectionSpec.metronome ?? false,
   };
 };
 
+const convertProject = async (
+  projectSpec: any,
+  uuid: string,
+  samplesDir: string
+): Promise<Project> => {
+  const project = emptyProject();
+  project.info = {
+    id: uuid,
+    name: projectSpec.name ?? 'Project',
+    version: '1',
+    lastSaved: Date.now(),
+  };
+
+  project.songs = projectSpec.songs
+    ? await Promise.all(
+        projectSpec.songs.map((songSpec: any) =>
+          convertSong(songSpec, samplesDir)
+        )
+      )
+    : [];
+
+  project.selections = {
+    song: project.songs[0].id,
+    section: project.songs[0].sections[0].id,
+  };
+
+  return project;
+};
+
+const writeProject = async (projectFile: string) => {
+  const spec = JSON.parse(fs.readFileSync(projectFile, 'utf-8'));
+
+  const projectUuid = randomUUID();
+
+  const projectsDir = path.join(os.homedir(), 'bloop', 'projects');
+  const projectDir = path.join(projectsDir, projectUuid);
+  const projectJson = path.join(projectDir, 'project.json');
+  const samplesDir = path.join(projectDir, 'samples');
+
+  fs.mkdirSync(projectDir);
+  fs.mkdirSync(samplesDir);
+
+  const project = await convertProject(spec, projectUuid, samplesDir);
+
+  fs.writeFileSync(projectJson, JSON.stringify(project, null, 2));
+
+  console.log('Project written to: ', projectDir);
+};
+
 const projectFile = process.argv[2];
-const spec = JSON.parse(fs.readFileSync(projectFile, 'utf-8'));
+if (!projectFile) {
+  console.error('Usage: npx ts-node [spec-file.json]');
+  process.exit(1);
+}
 
-const project = emptyProject();
-project.info = {
-  id: randomUUID(),
-  name: spec.name ?? 'Project',
-  version: '1',
-  lastSaved: Date.now(),
-};
-
-project.songs = spec.songs ? spec.songs.map(convertSong) : [];
-
-project.selections = {
-  song: project.songs[0].id,
-  section: project.songs[0].sections[0].id,
-};
-
-console.log(JSON.stringify(project, null, 2));
+writeProject(projectFile);
