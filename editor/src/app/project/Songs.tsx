8a9febce0d0933edaa00fe1232ab57be8a8647f0
Joe Noël <joe.noel@focusrite.com>
2023-11-20 14:09:18 +0000

    Fix issue where input would lose focus on typing

--- editor/src/app/project/Songs.tsx
+++ editor/src/app/project/Songs.tsx
@@ -1,9 +1,22 @@
-import {Divider, Stack} from '@mui/joy';
+import {Button, Divider, Stack} from '@mui/joy';
 import {useSongs} from '../../model-hooks/song-hooks';
 import {Song} from '../song/Song';
+import {Add} from '@mui/icons-material';
+import {useCore} from '../../core/use-core';
+import {addSongRequest} from '../../api/request';
 
 export const Songs = () => {
   const songs = useSongs();
+  const core = useCore();
+
+  if (!core) {
+    return <></>;
+  }
+
+  const addSong = () => {
+    const request = addSongRequest();
+    core.sendRequest(request);
+  };
 
   return (
     <Stack spacing={2}>
@@ -14,6 +27,12 @@ export const Songs = () => {
             <Divider />
           </Stack>
         ))}
+
+      <Stack direction="row">
+        <Button startDecorator={<Add />} onClick={addSong}>
+          Add Song
+        </Button>
+      </Stack>
     </Stack>
   );
 };
