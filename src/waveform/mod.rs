bb9684fd9a4207b6e2eb7f5ba37b83159cb1f9bd
Joe Noël <joe.noel@focusrite.com>
2023-12-16 13:20:54 +0000

    Make the generator module test only

--- src/waveform/mod.rs
+++ src/waveform/mod.rs
@@ -2,4 +2,4 @@ mod data;
 mod generate;
 
 pub use data::*;
-pub use generate::{generate_waveform_from_audio, generate_waveform_from_file, Options};
+pub use generate::{generate_waveform_from_file, Options};
