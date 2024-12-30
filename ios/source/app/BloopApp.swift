256d377eb3fd27bf3310c662306c0da420b0d47f
Joe Noël <joe@joenoel.co.uk>
2024-12-30 21:03:03 +0000

    Prevent sleep on iOS (#418)

--- ios/source/app/BloopApp.swift
+++ ios/source/app/BloopApp.swift
@@ -2,18 +2,33 @@ import SwiftUI
 
 @main
 struct BloopApp: App {
+    
+    @StateObject private var lifecycleHandler = AppLifecycleHandler()
 
     @State var store = Store(
         reducer: rootReducer,
         state: AppState(),
         middlewares: [ApiMiddleware(), WaveformMiddleware(), UploadMiddleware()]
     )
+    
+    init() {
+        lifecycleHandler.preventSleep = true
+    }
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
+                .environmentObject(lifecycleHandler)
 
+        }
+    }
+}
+ 
+class AppLifecycleHandler: ObservableObject {
+    @Published var preventSleep: Bool = false {
+        didSet {
+            UIApplication.shared.isIdleTimerDisabled = preventSleep
         }
     }
 }
