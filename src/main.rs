21fcde00f01f1076c54d805589899509ed74ec29
Joe Noël <joe@joenoel.co.uk>
2025-01-14 22:13:23 +0000

    Add basic iced UI

--- src/main.rs
+++ src/main.rs
@@ -12,20 +12,19 @@ mod types;
 mod ui;
 mod waveform;
 
+use crate::network::run_server;
 use control::run_main_controller;
 use git_version::git_version;
-use std::time::SystemTime;
-use ui::run_ui;
-
-use crate::network::run_server;
 use log::info;
+use std::thread;
+use std::time::SystemTime;
 use tokio::join;
 use tokio::sync::{broadcast, mpsc};
+use ui::run_ui;
 
 const GIT_SHA: &str = git_version!();
 
-#[tokio::main]
-async fn main() {
+fn main() {
     setup_logger().expect("Failed to setup logger");
 
     let version = env!("CARGO_PKG_VERSION");
@@ -35,10 +34,18 @@ async fn main() {
     let (request_tx, request_rx) = mpsc::channel(128);
     let (response_tx, _) = broadcast::channel(128);
 
-    let control = run_main_controller(request_rx, response_tx.clone());
-    let network = run_server(request_tx, response_tx);
-    let ui = run_ui();
-    join!(control, network, ui);
+    let async_task = thread::spawn(move || {
+        let runtime = tokio::runtime::Runtime::new().expect("Failed to create runtime");
+        runtime.block_on(async {
+            let control = run_main_controller(request_rx, response_tx.clone());
+            let network = run_server(request_tx, response_tx);
+            join!(control, network);
+        });
+    });
+
+    run_ui().expect("Error running UI");
+
+    async_task.join().expect("Failed to join async task");
 }
 
 fn setup_logger() -> Result<(), fern::InitError> {
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,1 +1,39 @@
-pub async fn run_ui() {}
+use std::time::Duration;
+
+use iced::widget::{button, text};
+use iced::{time, Element, Subscription, Theme};
+
+pub fn run_ui() -> iced::Result {
+    iced::application("Bloop", update, view)
+        .theme(theme)
+        .subscription(subscription)
+        .run()
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+#[derive(Default)]
+struct State {
+    counter: u64,
+}
+
+fn update(state: &mut State, message: Message) {
+    match message {
+        Message::Increment => state.counter += 1,
+    }
+}
+
+fn view(state: &State) -> Element<Message> {
+    button(text(state.counter)).on_press(Message::Increment).into()
+}
+
+fn theme(_state: &State) -> Theme {
+    Theme::Dark
+}
+
+fn subscription(_state: &State) -> Subscription<Message> {
+    time::every(Duration::from_millis(1_000)).map(|_| Message::Increment)
+}
