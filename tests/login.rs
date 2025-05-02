4bdea165d8d65d142a3fea38b45b6699df38046c
Joe Noël <joe.noel@zwift.com>
2025-05-02 13:01:00 -0700

    Re-factor use of user id in tests

--- tests/login.rs
+++ tests/login.rs
@@ -5,15 +5,13 @@ use chrono::DateTime;
 use common::BackendFixture;
 use serde_json::json;
 
-fn get_user_id() -> String {
-    "abc123".to_string()
-}
+fn user_json() -> (String, serde_json::Value) {
+    let id = "abc123".to_string();
 
-fn get_user_json() -> serde_json::Value {
-    serde_json::json!({
+    let user = serde_json::json!({
         "collectionId": "_pb_users_auth_",
         "collectionName": "users",
-        "id": get_user_id(),
+        "id": id,
         "email": "test@example.com",
         "emailVisibility": true,
         "verified": true,
@@ -21,11 +19,13 @@ fn get_user_json() -> serde_json::Value {
         "avatar": "filename.jpg",
         "created": "2022-01-01 10:00:00.123Z",
         "updated": "2022-02-03 10:00:00.123Z"
-    })
+    });
+
+    (id, user)
 }
 
-fn verify_user(user: &User) {
-    assert_eq!(user.id, get_user_id());
+fn verify_user(user: &User, id: &str) {
+    assert_eq!(user.id, id);
     assert_eq!(user.email, "test@example.com");
     assert!(user.email_visibility);
     assert_eq!(user.name, "test");
@@ -43,10 +43,11 @@ async fn test_successful_log_in() {
 
     let email = "user@abc.com";
     let password = "password";
+    let (id, user_json) = user_json();
 
     let response = json!(
         {
-            "record": get_user_json(),
+            "record": user_json,
             "token": "test_token"
         }
     );
@@ -61,7 +62,7 @@ async fn test_successful_log_in() {
     let result = fixture.backend.log_in(email.to_string(), password.to_string()).await;
 
     let user = result.unwrap();
-    verify_user(&user);
+    verify_user(&user, &id);
 
     login_mock.assert();
 }
@@ -90,20 +91,22 @@ async fn test_unsuccessful_log_in() {
 async fn test_get_user_successful() {
     let fixture = BackendFixture::new();
 
+    let (id, user_json) = user_json();
+
     let mock = fixture.mock_server.mock(|when, then| {
         when.method("GET")
-            .path(format!("/api/collections/users/records/{}", get_user_id()));
+            .path(format!("/api/collections/users/records/{}", id));
         then.status(200)
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
-            .json_body(get_user_json());
+            .json_body(user_json);
     });
 
-    let result = fixture.backend.get_user(&get_user_id()).await;
+    let result = fixture.backend.get_user(&id).await;
     assert!(result.is_ok());
 
     let user = result.unwrap();
-    verify_user(&user);
+    verify_user(&user, &id);
 
     mock.assert();
 }
@@ -112,16 +115,18 @@ async fn test_get_user_successful() {
 async fn test_get_user_unsuccessful() {
     let fixture = BackendFixture::new();
 
+    let (id, _) = user_json();
+
     let mock = fixture.mock_server.mock(|when, then| {
         when.method("GET")
-            .path(format!("/api/collections/users/records/{}", get_user_id()));
+            .path(format!("/api/collections/users/records/{}", id));
         then.status(404)
             .header("Content-Type", "application/json")
             .header("Accept", "application/json")
             .body(r#"{"status": 404,"message": "The requested resource wasn't found.","data": {}}"#);
     });
 
-    let result = fixture.backend.get_user(&get_user_id()).await;
+    let result = fixture.backend.get_user(&id).await;
     assert!(result.is_err());
 
     mock.assert();
