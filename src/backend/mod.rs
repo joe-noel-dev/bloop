91729f46f37d8a440510101985ccdfba66141efa
Joe Noël <joe@joenoel.co.uk>
2025-05-17 21:26:56 +0100

    Remove extra space

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -42,7 +42,6 @@ pub trait Backend {
     async fn add_project_sample(&self, project_id: &str, sample_bytes: &[u8], sample_name: &str) -> Result<DbProject>;
     async fn remove_project_sample(&self, project_id: &str, sample_name: &str) -> Result<DbProject>;
     async fn remove_project(&self, project_id: &str) -> Result<()>;
-
     async fn get_project_file(&self, project_id: &str, project_filename: &str) -> Result<Vec<u8>>;
 }
 
