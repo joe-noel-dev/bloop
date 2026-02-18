b934b40daf45cfe03aacf874dbc30107aada7d4a
Joe Noël <joe@joenoel.co.uk>
2026-02-18 18:18:46 +0000

    Make BUILD_NUMBER required in release script

--- scripts/release-ios.sh
+++ scripts/release-ios.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$BUILD_NUMBER" ]; then
+  echo "Error: BUILD_NUMBER environment variable is not set" >&2
+  exit 1
+fi
+
 PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
  
 cd ${PROJECT_ROOT}
